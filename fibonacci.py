71ed3053301cbf7b4923162095fe346110daedd9
Nick Meldrum <nick.meldrum@scribestar.com>
2015-01-04 11:41:45 +0000

    simplification making use of pythons variable assignment rules

--- fibonacci.py
+++ fibonacci.py
@@ -2,10 +2,8 @@ previousNumber = 0
 currentNumber = 1
 
 print previousNumber
-print currentNumber
 
 while currentNumber < 100:
-    nextNumber = currentNumber + previousNumber
-    print nextNumber
-    previousNumber = currentNumber
-    currentNumber = nextNumber
+    print currentNumber
+    previousNumber, currentNumber = currentNumber, currentNumber + previousNumber
+ 
