84f70565513d2d61a343ff5d004c14dde4b94c4d
Nick Meldrum <nick.meldrum@scribestar.com>
2015-01-08 09:06:56 +0000

    added new fibonnaci func that returns a list instead of printing

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,19 @@
-previousNumber, currentNumber = 0, 1
-print previousNumber
+def fib():
+ previousNumber, currentNumber = 0, 1
+ print previousNumber,
 
-while currentNumber < 100:
-    print currentNumber
-    previousNumber, currentNumber = currentNumber, currentNumber + previousNumber
+ while currentNumber < 100:
+  print currentNumber,
+  previousNumber, currentNumber = currentNumber, currentNumber + previousNumber
  
+def fib2(n):
+ result = []
+ a, b = 0, 1
+ while a < n:
+  result.append(a)
+  a, b = b, a+b
+ return result
+
+fib()
+print fib2(100)
+
